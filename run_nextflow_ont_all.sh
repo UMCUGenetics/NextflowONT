c14c29ff2a36756a813a24b419053468bddeeb9f
melferink <m.elferink@umcutrecht.nl>
2023-04-03 09:12:16 +0200

    refactor + method wgs_repeat

--- run_nextflow_ont_all.sh
+++ run_nextflow_ont_all.sh
@@ -4,60 +4,67 @@ set -euo pipefail
 workflow_path='/hpc/diaggen/software/development/NextflowONT'
 
 # Set input and output dirs
-input_fast5=`realpath -e $1`
+input_path=`realpath -e $1`
 output=$2
 email=$3
 sample_id=$4
-method=${5-default}
+start_run=$5
+method=${6-default}
 
 
 if [ $method == "wgs" ]; then
     echo " #### Running method wgs  ####"
 fi
 
+if [ $method == "wgs_repeat"  ]; then
+    echo " #### Running method wgs_repeat  ####"
+    strique_config='--strique_config '$7
+fi
 
 if [ $method == "wgs_roi" ]; then
     echo " #### Running method wgs + roi phasing  ####"
-    roi='--roi '$6
+    roi='--roi '$7
 fi
 
 
 if [ $method == "wgs_roi_repeat" ]; then
     echo " #### Running method wgs + roi phasing + repeat calling ####"
-    roi='--roi '$6
-    strique_config='--strique_config '$7
+    roi='--roi '$7
+    strique_config='--strique_config '$8
 fi
 
 
 if [ $method == "wgs_splitcas9_repeat" ]; then
     echo " #### Running method wgs + split cas9 ####"
     splitfile='--splitfile '$7
+    strique_config='--strique_config '$8
 fi
 
 
 if [ $method == "targeted" ]; then
     echo " #### Running method wgs + targeted mapping  ####"
-    roi='--roi '$6
+    roi='--roi '$7
 fi
 
 
 if [ $method == "targeted_splitcas9" ]; then
     echo " #### Running method targeted + split cas9  ####"
-    roi='--roi '$6
-    splitfile='--splitfile '$7
+    roi='--roi '$7
+    splitfile='--splitfile '$8
 fi
 
 
 if [ $method == "targeted_SMA_splitcas9" ]; then
     echo " #### Running method targeted SMA specific + split cas9  ####"
-    roi='--roi '$6
-    splitfile='--splitfile '$7
+    roi='--roi '$7
+    splitfile='--splitfile '$8
 fi
 
 
 if [ $method == "targeted_SMA_adaptive" ]; then
     echo " #### Running method targeted SMA specific + adaptive sequencing ####"
-    roi='--roi '$6
+    roi='--roi '$7
+    ploidy='--ploidy '$8
 fi
 
 
@@ -85,14 +92,16 @@ module load Java/1.8.0_60
 
 /hpc/diaggen/software/tools/nextflow run $workflow_path/ONT_wgs_all.nf \
 -c $workflow_path/ONT_wgs_all.config \
---fast5_path $input_fast5 \
+--input_path $input_path \
 --outdir $output \
 --email $email \
 --sample_id $sample_id \
+--start $start_run \
 --method $method \
 ${roi:-""} \
 ${strique_config:-""} \
 ${splitfile:-""} \
+${ploidy:-""} \
 -profile slurm \
 -resume -ansi-log false
 
