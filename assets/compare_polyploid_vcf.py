0f3a2bdd9b1f93bdf6060aeb44024d7f92d04a31
melferink <m.elferink@umcutrecht.nl>
2023-09-28 15:56:29 +0200

    included ploidy

--- assets/compare_polyploid_vcf.py
+++ assets/compare_polyploid_vcf.py
@@ -33,12 +33,10 @@ def ParseIllumina(illumina_vcf, min_ad_illumina):
 
 
 def CompareONT(ont_vcf, illumina_dic, high_conf, low_conf):
+    print(f"Conclusion\tIllumina_Chr\tIllumina_Position\tIllumina_GT\tIllumina_AF\tONT_Chr\tONT_Position\tONT_GT\tONT_AF")
     vcf_reader = vcf.Reader(open(ont_vcf, 'rb'))
     sampleid = vcf_reader.samples[0]  ## Assume single sample VCF here!
-
-    tp_high = tp_low = fn = unknown = 0
-
-    print(f"Conclusion\tIllumina_Chr\tIllumina_Position\tIllumina_GT\tIllumina_AF\tONT_Chr\tONT_Position\tONT_GT\tONT_AF")
+    tp_high = tp_low = fn = fn_homopolymer = unknown = 0
     for region in illumina_dic:
         ill_chrom, ill_pos = region.split("_")
         ill_gt = illumina_dic[region]["gt"]
@@ -52,45 +50,55 @@ def CompareONT(ont_vcf, illumina_dic, high_conf, low_conf):
             ad = record.genotype(sampleid)['AD']
             af = ad[1] / (ad[0] + ad[1]) * 100
             if not illumina_dic[region]["ishet"] and not record.genotype(sampleid).is_het:  # Both Illumina and ONT are homozygous
-                print(f"TP_homozygous\t\t{ill_chrom}\t{ill_pos}\t{ill_gt}\t{ill_af:.2f}\t{chrom}\t{pos}\t{gt}\t{af}") 
+                print(f"TP_homozygous\t\t{ill_chrom}\t{ill_pos}\t{ill_gt}\t{ill_af:.2f}\t{chrom}\t{pos}\t{gt}\t{af:.2f}") 
                 tp_high += 1
             else:
                 if af > (ill_af - high_conf) and af < (ill_af + high_conf):
-                    print(f"TP_heterozygous_{high_conf}\t\t{ill_chrom}\t{ill_pos}\t{ill_gt}\t{ill_af:.2f}\t{chrom}\t{pos}\t{gt}\t{af}")
+                    print(f"TP_heterozygous_high_conf\t\t{ill_chrom}\t{ill_pos}\t{ill_gt}\t{ill_af:.2f}\t{chrom}\t{pos}\t{gt}\t{af:.2f}")
                     tp_high += 1
                 elif af > (ill_af - low_conf) and af < (ill_af + low_conf):
-                    print(f"TP_heterozygous_{low_conf}\t\t{ill_chrom}\t{ill_pos}\t{ill_gt}\t{ill_af:.2f}\t{chrom}\t{pos}\t{gt}\t{af}")
+                    print(f"TP_heterozygous_low_conf\t\t{ill_chrom}\t{ill_pos}\t{ill_gt}\t{ill_af:.2f}\t{chrom}\t{pos}\t{gt}\t{af:.2f}")
                     tp_low += 1
                 else:
                     if ill_filter:
                         ill_filters  = "_".join(ill_filter)
-                        print(f"Possible_Filter_{ill_filters}\t\t{ill_chrom}\t{ill_pos}\t{ill_gt}\t{ill_af:.2f}\t{chrom}\t{pos}\t{gt}\t{af}")
+                        print(f"Possible_Filter_{ill_filters}\t\t{ill_chrom}\t{ill_pos}\t{ill_gt}\t{ill_af:.2f}\t{chrom}\t{pos}\t{gt}\t{af:.2f}")
                         unknown += 1
                     else:
-                        print(f"Unknown\t\t{ill_chrom}\t{ill_pos}\t{ill_gt}\t{ill_af:.2f}\t{chrom}\t{pos}\t{gt}\t{af}")
+                        print(f"Unknown\t{ill_chrom}\t{ill_pos}\t{ill_gt}\t{ill_af:.2f}\t{chrom}\t{pos}\t{gt}\t{af:.2f}")
                         unknown += 1
 
         if not pos:  # Variant not detected
             if ill_filter:
                 ill_filters  = "_".join(ill_filter)
                 print(f"FN_ONT_{ill_filters}\t{ill_chrom}\t{ill_pos}\t{ill_gt}\t{ill_af:.2f}\tn/a\tn/a\tn/a\tn/a")
-                unknown += 1
+                if "Homopolymer" in ill_filters:
+                    fn_homopolymer += 1
+                else:
+                    unknown += 1
             else:
                 print(f"FN_ONT\t{ill_chrom}\t{ill_pos}\t{ill_gt}\t{ill_af:.2f}\tn/a\tn/a\tn/a\tn/a")
                 fn += 1
-    print(f"TP_high\tTP_low\tTP_total\tFN\tUnknown\tTotal")
-    print(f"{tp_high}\t{tp_low}\t{tp_high + tp_low}\t{fn}\t{unknown}\t{tp_high + tp_low + fn + unknown}")
+    print(f"TP_high\tTP_low\tTP_total\tFN\tFN_due_to_homopolymer\tUnknown\tTotal")
+    print(f"{tp_high}\t{tp_low}\t{tp_high + tp_low}\t{fn}\t{fn_homopolymer}\t{unknown}\t{tp_high + tp_low + fn + fn_homopolymer + unknown}")
  
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('illumina_vcf', help='Full path to Illumina VCF')
     parser.add_argument('ont_vcf', help='Full path to ONT polyploid VCF')
     parser.add_argument('--min_ad_illumina', default= 30, type=int, help='Minimum Depth Illumina to be considered usefull variant')
-    parser.add_argument('--high_conf', default= 10, type=int, help='Minimum Depth Illumina to be considered usefull variant')
-    parser.add_argument('--low_conf', default= 20, type=int, help='Minimum Depth Illumina to be considered usefull variant')
+    parser.add_argument('--high_conf', default= 10, type=int, help='percentage range for high confident af')
+    parser.add_argument('--low_conf', default= 20, type=int, help='percentage range for lower confident af')
+    parser.add_argument('--ploidy', type=int, help='ploidy of sample. Is used to calculate high and low confident af')
     args = parser.parse_args()
 
+    high_conf = args.high_conf
+    low_conf = args.low_conf
+    if args.ploidy:
+        high_conf = 100 / args.ploidy / 2
+        low_conf = 100 / args.ploidy 
+
     illumina_dic = ParseIllumina(args.illumina_vcf, args.min_ad_illumina)
-    CompareONT(args.ont_vcf, illumina_dic, args.high_conf, args.low_conf)
+    CompareONT(args.ont_vcf, illumina_dic, high_conf, low_conf)
 
 
